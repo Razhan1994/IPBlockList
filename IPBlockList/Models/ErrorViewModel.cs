85d68486aa4f9a5d0c31d14fb2e26f35142c1b8e
JavidKoluRazhan <javidklurajan@gmail.com>
2021-05-19 11:05:22 +0430

    .

---
+++ IPBlockList/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace IPBlockList.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
